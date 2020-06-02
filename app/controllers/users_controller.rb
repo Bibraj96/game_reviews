1db6ad62961e1334451e050c19a1f8eb340ed7d8
bibraj96 <blendi.ibraj96@gmail.com>
2020-06-02 13:05:24 -0400

    create login form in login.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
-  #renders login form
+  # renders login form
   get '/login' do
     erb :login
+  end
+
+  # receive the login form, find the user, log the user in by creating a session
+  post '/login' do
+
   end
 
   get '/signup' do
