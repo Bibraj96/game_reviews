9c0eecf0ed1c84a388f77113b5719bce26cd3b62
bibraj96 <blendi.ibraj96@gmail.com>
2020-06-02 15:32:43 -0400

    create logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,13 @@ class UsersController < ApplicationController
 
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
+
     erb :'/users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 
 end
