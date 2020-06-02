def381338c99a1ad87912fdf178cfe2592420beb
bibraj96 <blendi.ibraj96@gmail.com>
2020-06-02 13:17:53 -0400

    enable sessions and set sessions secret in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "game_reviews_app" # Extra layer of security, someone with the wrong session secret doesn't have access
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,17 @@ class UsersController < ApplicationController
   end
 
   # receive the login form, find the user, log the user in by creating a session
+  # find the user
+  # authenticate the user(verify user is who they say they are)
+  # log the user in (create session)
+  # redirect to the user's landing page
   post '/login' do
-
+    @user = User.find_by(email: params[:email]) #Finds user
+    if @user.authenticate(params[:password])
+      
+    else
+      # tell the user they entered invalid credentials
+      #redirect to the login page
   end
 
   get '/signup' do
