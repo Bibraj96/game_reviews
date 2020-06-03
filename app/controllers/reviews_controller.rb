aab89ae2a0c6158c52086a7f065c355f15dd2a59
bibraj96 <blendi.ibraj96@gmail.com>
2020-06-03 14:57:15 -0400

    add controller action to display a user's reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,11 @@ class ReviewsController < ApplicationController
     end
   end
 
+  get '/reviews' do
+    @reviews = Review.all
+    erb :'/reviews/index'
+  end
+
   get '/reviews/:id' do
     @review = Review.find(params[:id])
     erb :'/reviews/show'
