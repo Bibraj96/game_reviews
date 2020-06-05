eee086b74c357332f2201c1421d29f322181a3ad
bibraj96 <blendi.ibraj96@gmail.com>
2020-06-05 11:25:29 -0400

    add validation for name, email, and email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   
   has_many :reviews
+
   has_secure_password #allows us to use authenticate method which checks an inputted password and compares it with bcrypt's encrypted password
-  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  # these validation requirements need to be met in order to create, save, or update a new user
 end
